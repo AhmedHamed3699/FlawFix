1b98b7fd4ed8558c78dbf99785d8c3d2e4f0ffff
somiaelshemy <somiaelshemy22@gmail.com>
2024-12-22 20:46:07 +0200

    main: add main function

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
-def hello_world():
-    print("Hello, world!")
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
 
-if __name__ == '__main__':
-    hello_world()
+from core.utils.preprocessing import adjust_brightness_to_range
+from core.utils.common_functions import show_images
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_image", help="📷 Path to the input image")
+    parser.add_argument("-o", "--output", help="💾 Path to save the adjusted image (optional)")
+
+    args = parser.parse_args()
+
+    print("\n✨ Welcome to the FlawFix! ✨")
+    print(f"📂 Input Image: {args.input_image}")
+    if args.output:
+        print(f"💾 Output Image will be saved at: {args.output}")
+    else:
+        print("👀 The adjusted image will be displayed (not saved).")
+
+    print("\n🛠️ Processing your image... Please wait...\n")
+
+    image = Image.open(args.input_image)
+
+    ################################ PREPROCESSING ################################
+    brightness_adjusted_image = adjust_brightness_to_range(image)
+
+    show_images([brightness_adjusted_image], ['brightness_adjusted_image'])
+
+
+    ######################## Write Your Function Calls Here #######################
+
+
+    ################################################################################
+    print("\nFinished Processing.\n")
+
+if __name__ == "__main__":
+    main()
